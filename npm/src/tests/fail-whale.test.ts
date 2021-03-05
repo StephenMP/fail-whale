05ee83c01bb4b80c6d713673ef9555e80ed53db2
Spencer Zepelin <spencer.zepelin@mastery.net>
2021-03-05 15:32:02 -0600

    optionally inject logger (#1)
    Co-authored-by: Stephen Porter <1.stephen.porter@gmail.com>

--- npm/src/tests/fail-whale.test.ts
+++ npm/src/tests/fail-whale.test.ts
@@ -4,8 +4,21 @@ describe('print test', () => {
     it('can print without message', () => {
         console.error = jest.fn();
         console.log = jest.fn();
-        failWhale('hello');
-        expect(console.error).toHaveBeenCalled();
+        failWhale();
+        expect(console.error).toHaveBeenCalledTimes(4);
         expect(console.log).not.toHaveBeenCalled();
+    });
+
+    it('can take custom logger', () => {
+        const logWarn = {
+            error: (msg: any) => console.warn(msg)
+        }
+        logWarn.error = jest.fn();
+        console.error = jest.fn();
+        console.log = jest.fn();
+        failWhale('hello', logWarn);
+        expect(console.error).not.toHaveBeenCalled();
+        expect(console.log).not.toHaveBeenCalled();
+        expect(logWarn.error).toHaveBeenCalledTimes(5);
     });
 });
